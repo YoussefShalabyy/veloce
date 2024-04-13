1887c60b9f0cdeb8c4c3b4ebef0fc4cd6ed2a204
Yohana248 <152048862+Yohana248@users.noreply.github.com>
2024-04-13 03:06:11 +0200

    Separate the config of firebase in another file

--- firebase.js
+++ firebase.js
@@ -1,20 +1,9 @@
+import { app } from "./firebaseConfig";
+
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB-LehAYrlcTQcbhAFbctBBkRHYEJQ2uKk",
-  authDomain: "stickersmash-7cbdb.firebaseapp.com",
-  projectId: "stickersmash-7cbdb",
-  storageBucket: "stickersmash-7cbdb.appspot.com",
-  messagingSenderId: "264301268175",
-  appId: "1:264301268175:web:0620601e11b68c61617bba",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); // Get the authentication object
 const db = getFirestore(app);
 
