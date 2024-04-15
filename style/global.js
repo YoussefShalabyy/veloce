7f6c0d7346dd4fb549b30d7c8a4725eac8e8c96d
Yohana248 <152048862+Yohana248@users.noreply.github.com>
2024-04-15 09:38:43 +0200

    Make container and label global styles

---
+++ style/global.js
@@ -0,0 +1,15 @@
+const GlobalStyles = {
+    container: {
+        display: 'flex',
+        backgroundColor: Colors.light.backgroundcolor,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    label: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+};
+
+export default GlobalStyles;
