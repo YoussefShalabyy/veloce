f68a22db975aefe983851b03bcb3d2e779d47f01
Abdelrahman Ezzat Saeed <abdelrahman_ae@icloud.com>
2024-05-08 05:31:33 +0300

    Added Single Item Listing And changed the whole style

--- app/cars/[id].js
+++ app/cars/[id].js
@@ -23,65 +23,74 @@ export default function OneCar() {
     </View>
   );
 
+  const InfoHeading = ({ title }) => (
+    <View style={styles.subheadView}>
+      <Text style={styles.InfoItemTitle}>{title}</Text>
+    </View>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
+      <View style={styles.TopNavView}>
+        <Text style={styles.CarName}>{item.name}</Text>
+        <TouchableOpacity
+          style={styles.goBackBtn}
+          onPress={() => {
+            router.back();
+          }}
+        >
+          <Text style={styles.goBackText}>{">"}</Text>
+        </TouchableOpacity>
+      </View>
       <ScrollView
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
       >
-        <View>
-          <TouchableOpacity style={styles.goBackBtn}>
-            <Text>Go back</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.goBackBtn}>
-            <Text>Go back</Text>
-          </TouchableOpacity>
-        </View>
         <View style={styles.innerContainer}>
           <View style={styles.imageView}>
             <Image source={{ uri: item.images[0] }} style={styles.image} />
           </View>
-          <InfoItemDisplay title="Brand" value={item.brand} />
-          <InfoItemDisplay title="Name" value={item.name} />
-          <InfoItemDisplay title="Year" value={item.year} />
-          <InfoItemDisplay title="Color" value={item.color} />
-          <InfoItemDisplay title="Body Type" value={item.bodyType} />
-          <InfoItemDisplay title="Condition" value={item.condition} />
-          <InfoItemDisplay title="Transmission" value={item.transmission} />
-          <InfoItemDisplay
-            title="Transmission
-        Name"
-            value={item.transmissionName}
-          />
-          <InfoItemDisplay title="Fuel Type" value={item.fuelType} />
-          <InfoItemDisplay title="Displacement" value={item.displacement} />
-          <InfoItemDisplay title="Liter" value={item.liter} />
-          <InfoItemDisplay title="Horse Power" value={item.horsePower} />
-          <InfoItemDisplay title="Torque" value={item.torque} />
-          <InfoItemDisplay title="Top Speed" value={item.topSpeed} />
-          <InfoItemDisplay title="Acceleration" value={item.acceleration} />
-          <InfoItemDisplay
-            title="Fuel Efficiency"
-            value={item.fuelEfficiency}
-          />
-          <InfoItemDisplay title="Mileage" value={item.mileage} />
-          <InfoItemDisplay title="Location" value={item.location} />
-          <InfoItemDisplay title="Price" value={item.price} />
-          <InfoItemDisplay title="Warranty" value={item.warranty} />
+          <View style={styles.carInfo}>
+            <InfoItemDisplay title="Brand" value={item.brand} />
+            <InfoItemDisplay title="Name" value={item.name} />
+            <InfoItemDisplay title="Year" value={item.year} />
+            {/* <InfoItemDisplay title="Color" value={item.color} /> */}
+            <InfoItemDisplay title="Body Type" value={item.bodyType} />
+            {/* <InfoItemDisplay title="Condition" value={item.condition} /> */}
+            <InfoItemDisplay title="Gears" value={item.transmission} />
+            <InfoItemDisplay
+              title="Transmission
+        Type"
+              value={item.transmissionName}
+            />
+            <InfoItemDisplay title="Fuel Type" value={item.fuelType} />
+            <InfoHeading title="Engine" />
+            <InfoItemDisplay title="Displacement" value={item.displacement} />
+            <InfoItemDisplay title="Liter" value={item.liter} />
+            <InfoItemDisplay title="Horse Power" value={item.horsePower} />
+            <InfoItemDisplay title="Torque" value={item.torque} />
+            <InfoItemDisplay title="Top Speed" value={item.topSpeed} />
+            <InfoItemDisplay title="Acceleration" value={item.acceleration} />
+            <InfoItemDisplay
+              title="Fuel Efficiency"
+              value={item.fuelEfficiency}
+            />
+            <InfoHeading title="Car Details" />
+            <InfoItemDisplay title="Mileage" value={item.mileage} />
+            <InfoItemDisplay title="Location" value={item.location} />
+            {/* <InfoItemDisplay title="Warranty" value={item.warranty} /> */}
+            <InfoHeading title="About Car" />
+            <InfoItemDisplay title="" value={item.description} />
+            <InfoHeading title="Price" />
+            <InfoItemDisplay title="Price/Day" value={item.price + "$"} />
+          </View>
           <TouchableOpacity
-            style={styles.button}
+            style={styles.bookButton}
             onPress={() => {
               router.navigate("booking", { item: item });
             }}
           >
-            <Text style={styles.buttonText}>Book Now</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              router.back();
-            }}
-          >
-            <Text>Go back</Text>
+            <Text style={styles.bookButtonText}>Release The Beast</Text>
           </TouchableOpacity>
         </View>
       </ScrollView>
@@ -103,18 +112,43 @@ const styles = StyleSheet.create({
   },
   innerContainer: {
     flex: 1,
+    paddingHorizontal: 20,
     minWidth: "100%",
     justifyContent: "center",
     alignContent: "center",
     alignItems: "center",
   },
+  TopNavView: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    paddingHorizontal: 20,
+  },
+  CarName: {
+    fontSize: 30,
+    fontWeight: "bold",
+    maxWidth: "75%",
+    flex: 1,
+  },
   goBackBtn: {
     backgroundColor: Colors.dark.backgroundcolor,
-    padding: 10,
     borderRadius: 10,
     marginBottom: 10,
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 100,
+  },
+  goBackText: {
+    color: Colors.light.whiteBackground,
+    fontSize: 30,
+    fontWeight: "bold",
   },
   imageView: {
+    backgroundColor: Colors.main.orange,
     alignItems: "center",
     width: "100%",
     padding: 10,
@@ -126,14 +160,50 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     resizeMode: "contain",
   },
+  carInfo: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 20,
+  },
   InfoItem: {
     flexDirection: "row",
     justifyContent: "space-between",
-    width: "80%",
+    width: "90%",
+
     marginBottom: 10,
   },
   InfoItemTitle: {
     fontSize: 18,
+    fontWeight: "bold",
+  },
+  subheadView: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 10,
+    backgroundColor: Colors.main.orange,
+    borderRadius: 10,
+    minWidth: "90%",
+    margin: 10,
+  },
+  InfoItemValue: {
+    fontSize: 18,
+    fontWeight: "600",
+    padding: 3,
+  },
+  bookButton: {
+    backgroundColor: Colors.dark.backgroundcolor,
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+    marginTop: 20,
+    minWidth: "90%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  bookButtonText: {
+    color: Colors.light.whiteBackground,
+    fontSize: 16,
     fontWeight: "bold",
   },
 });
