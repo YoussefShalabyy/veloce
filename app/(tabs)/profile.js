b95432b16dd426566d3101028f3c9af396c6df0f
Abdelrahman Ezzat Saeed <abdelrahman_ae@icloud.com>
2024-05-10 03:08:33 +0300

    fixed some styles for responsive design

--- app/(tabs)/profile.js
+++ app/(tabs)/profile.js
@@ -227,6 +227,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignContent: "center",
     alignItems: "center",
+    paddingBottom : Platform.OS === "ios" ? 0 : 20,
   },
   HeaderView: {
     paddingHorizontal: 20,
